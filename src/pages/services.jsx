a75d3438588b4010903582811ab68ba00dec6142
German G <glezrdg@gmail.com>
2023-02-09 21:12:37 -0400

    layout design section

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,23 +1,7 @@
 import React from "react";
 
 const Services = () => {
-  return (
-    <div className="container mx-auto">
-      <div className="">
-        <h1>My Skill depends on work.</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
-          provident dolorem reprehenderit perspiciatis.{" "}
-        </p>
-      </div>
-      <div>
-        <div className="">
-          <p>icono</p>
-          <h1>Patient safety</h1>
-        </div>
-      </div>
-    </div>
-  );
+  return <></>;
 };
 
 export default Services;
